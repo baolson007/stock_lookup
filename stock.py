d5432791b0726da068bbee67f5380dc0ace17ec7
Ben Olson <benolson3154@gmail.com>
2021-10-27 14:58:35 -0700

    minor refactoring, and documentation added

--- stock.py
+++ stock.py
@@ -1,4 +1,4 @@
-from tkinter import *
+# Library to help simplify functions utilizing yahoo finance data
 import yahoo_fin.stock_info as stock_info
 import yahoo_fin as yf
 import pandas as pd
@@ -6,6 +6,8 @@ import pandas as pd
 dates = []
 prices = []
 
+# get all available price data, load dates and prices into 
+# class variables for future use.
 def get_historic_price_data( ticker ):
 	ticker_data = stock_info.get_data( ticker )# ... start_date = '09-24-2021', end_date = ''...)
 
@@ -19,23 +21,19 @@ def get_historic_price_data( ticker ):
 		prices.append( round( price, 2 ) )
 	
 	name = stock_info.get_quote_data( ticker ).get( 'shortName' )
-
-
-	print( "Price history for: " + name + " (" + ticker + ") loaded" )
 	return ( dates, prices )
 
+# retrieve a company's name given its stock symbol.
+def get_name( ticker ):
+	name = stock_info.get_quote_data( ticker ).get( 'shortName' )
+	return name
+
+# retrieve current price for the given stock symbol.
+# returns 'NA' if symbol does not able to be retreived 
+# by the yahoo_fin API
 def get_price( ticker ):
 	try:
 		price = round( stock_info.get_live_price( ticker ), 2 )
 		return price
 	except:
 		return 'NA'
-
-#try:
-	#ticker = input( "Please enter a valid ticker symbol: " ).upper()
-	#get_historic_price_data( ticker )
-	#print( "Current price for " + ticker + ": $" + str( get_price( ticker ) ) )
-#except:
-	#print( "Error processing request for stock ticker \'" + ticker + " \' try again" )
-
-
--- stockGUI.py
+++ stockGUI.py
@@ -1,39 +1,41 @@
-import tkinter as tk
-from stock import get_price
+# Simple GUI using tkinter to retreive and display a given company's
+# stock price when provided with the company's stock ticker. 
 
+import tkinter as tk
+from stock import get_name, get_price
+
+# Populates the result field of the window with the result
+# of the get name/price calls for the given stock
 def on_click(event=None):
    ticker = ent_ticker.get()
    price = get_price( ticker )
+   name = get_name( ticker )
+
    if price != 'NA':
-      lbl_result['text'] = "$" + str( price )
+      lbl_result['text'] = name + ": $" + str( price )
    else:
       lbl_result['text'] = ticker + " is an invalid ticker, try again"
    
+# tkinter setup
 root = tk.Tk()
 root.title("Stock Price Finder")
 root.geometry('400x300')
 root.bind('<Return>', on_click)
 
-lbl = tk.Label( root, text='Please enter a stock ticker symbol' )
-
+# widgets
+lbl_welcome = tk.Label( root, text='Welcome!' )
+lbl_entry = tk.Label( root, text='Enter stock symbol (ex:GOOG): ' )
 ent_ticker = tk.Entry(root, width=15)
-#txt.grid( row = 0, column = 1)
 btn_submit = tk.Button( root, text="look up price", width=10, command=on_click )
-
 lbl_result = tk.Label( text="result" )
 btn_exit = tk.Button( root, text="Exit Program", command=root.destroy )
 
-lbl.pack()
-ent_ticker.pack()
-btn_submit.pack()
-lbl_result.pack()
-btn_exit.pack()
-
-
-
-
-#quote = stock.get_price(ticker)
-
-#quote.pack()
+# formatting
+lbl_welcome.grid( row=1, column=1, pady=10 )
+lbl_entry.grid( row=2, column=1 )
+ent_ticker.grid( row=2, column=2, padx=5, sticky='N' )
+btn_submit.grid( row= 3, column=1, pady=10 )
+lbl_result.grid( row= 4, column=1, pady=10 )
+btn_exit.grid( row= 7, column=1, padx=5, pady=20, sticky='N' )
 
 root.mainloop()
